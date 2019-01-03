6dc9ccc5c06800d3e1ab5bcc7fa3e43d9fe6b3a0
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-03 23:39:30 +0000

    challenge 4

--- Challenge 4/solution/script.js
+++ Challenge 4/solution/script.js
@@ -7,14 +7,15 @@ Error after the line 20 should still be generated.
 "use strict";
 
 var a = 5,
-  b = 10;
+    b = 10;
 
 if (b > a) {
-  c = a + b + c;
-  let c = 2;
-  console.log(c);
-  // BEFORE: Uncaught ReferenceError: c is not defined
-  // AFTER: 17
+    let c = 2;
+    c = a + b + c;
+
+    console.log(c);
+    // BEFORE: Uncaught ReferenceError: c is not defined
+    // AFTER: 17
 }
 
 console.log(c);
