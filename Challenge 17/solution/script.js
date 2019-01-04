f5493f57739d0394f91c1f2c6a904e077c6022c1
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-04 23:36:04 +0000

    challenge 17

--- Challenge 17/solution/script.js
+++ Challenge 17/solution/script.js
@@ -1,23 +1,40 @@
-/* CHALLENGE 17 - IIFE (Immediately Invoked Function Expression)
+// /* CHALLENGE 17 - IIFE (Immediately Invoked Function Expression)
 
-Complete IIFE that will expose following methods:
-  greet() - It will take one argument and return greeting string
-  changeGreeting() - It will change greeting string
+// Complete IIFE that will expose following methods:
+//   greet() - It will take one argument and return greeting string
+//   changeGreeting() - It will change greeting string
 
-Initial greeting string "Hey, that's" must be defined inside IIFE.
-*/
+// Initial greeting string "Hey, that's" must be defined inside IIFE.
+// */
 
 "use strict";
 
-// var greeting = (Write IIFE here);
 
-console.log(greeting.greet("Bob"));
-// Hey, that's Bob
+const greeting = (() => {
+    let string = "Hey, that's";
 
-console.log(
-  greeting.changeGreeting("Good Morning from")
-);
-// undefined
+    function greet(name) {
+        return `${string} ${name}`
+    }
 
-console.log(greeting.greet("Emily"));
-// Good Morning from Emily
+    function changeGreeting(newGreeting) {
+        string = newGreeting;
+    }
+    return {
+        greet,
+        changeGreeting
+    };
+
+})();
+
+// Usage:
+
+var singleA = greeting.greet("Bob");
+console.log(singleA);
+var singleB = greeting.changeGreeting("Good morning from");
+var singleD = greeting.greet("george");
+console.log(singleD);
+// Note: as we are working with random numbers, there is a
+// mathematical possibility both numbers will be the same,
+// however unlikely. The above example should otherwise still
+// be valid.
