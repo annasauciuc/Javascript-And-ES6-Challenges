5d8bac062bc2d3d1c5db487657ccf06d0808d3f7
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-04 02:31:42 +0000

    challenhe 7

--- Challenge 7/solution/script.js
+++ Challenge 7/solution/script.js
@@ -3,6 +3,7 @@
 Answer following question:
 1. Why on the line 14 we can't simply use following statement:
 mult = mult || 2;
+Because  we want 'mult' to be 2 when is not declared, when is undefined 
 
 Set default value of the mult parameter
 in the multiplyBy() function.
@@ -10,15 +11,14 @@ in the multiplyBy() function.
 
 "use strict";
 
-function multiplyBy(a, mult) {
-  mult = mult !== undefined ? mult : 2;
-  console.log(a * mult);
+function multiplyBy(a, mult = 2) {
+    console.log(a * mult);
 }
 
 multiplyBy(2);
 // 4
 
-multiplyBy(2, undefined);
+multiplyBy(2);
 // 4
 
 multiplyBy(2, 0);
